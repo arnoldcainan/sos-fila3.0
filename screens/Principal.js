4ac7effe1dc1f655d4b9cddd38806a7f82e37e0c
arnoldcainan <86830636+arnoldcainan@users.noreply.github.com>
2023-03-05 11:20:01 -0300

    Padrao

--- screens/Principal.js
+++ screens/Principal.js
@@ -1,13 +1,61 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons'; // icone local
+import { AntDesign } from '@expo/vector-icons';  //icone relogio
 
 
-export default function Principal() {
-  return (
-    <View>
-        <Text>Tela de Cards</Text>
-    </View>
+const Principal = ({ navigation, route }) => {
+ return (
+
+    <View >
+     <Text>Hello</Text>
+    </View>   
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    borderRadius: 30,
+    backgroundColor: '#E0FFFF',
+  },
+  //espaço texto
+  paragraph: {
+    margin: 10,
+    marginTop: 30,
+    marginBottom: 5,
+    fontSize: 27,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  //texto
+  texto:{
+    paddingTop: 0,
+    margin: 2,
+    marginTop: 10,
+    marginBottom: 4,
+    fontSize: 15,
+    fontWeight: 'normal',
+    textAlign: 'center',
+  },
+  //imagems
+  logo: {
+    left:-70,
+    height: 100,
+    width: 100,    
+    marginVertical:5,
+    marginBottom: 5,
+  },
+  //icones
+  viewIcon: { 
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: 20,
+    marginTop: -170,
+    marginBottom: 20,
+    marginRight:-200,
+},
+});
 
+export default Principal
