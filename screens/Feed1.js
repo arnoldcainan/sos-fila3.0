c9938761d9d14997654b0edc6cae07fa8b5b88aa
Harlei Akira Ohta <harlei.akira@outlook.com>
2023-03-07 12:10:20 -0300

    cards

--- screens/Feed1.js
+++ screens/Feed1.js
@@ -13,76 +13,164 @@ import { Text, Card, Button } from '@rneui/themed';
 
 const Feed1 = ({ navigation, route }) => {
   return (
-    <ScrollView>
-      <Text style={{ flex: 1, flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
-        <Image style={styles.image}
-          source={require('../logo/joel.png')}
-        />  Joel Miller</Text>
-      <Card.Divider />
-      <View style={styles.container}>
+    <>
+      <ScrollView>
         <Card>
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.navigate('Card1')}>
+          <Text
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginLeft: 10,
+              marginTop: 20,
+              marginBottom: 20,
+              fontWeight: 'bold',
+            }}>
             <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-            <View></View>
-          </TouchableOpacity>
-        </Card>
+              style={styles.image}
+              source={require('../assets/joel.png')}
+            />{' '}
+            Joel Miller
+          </Text>
 
+          <Card.Divider />
+          <View style={styles.container}>
+            <View>
+              <TouchableOpacity
+                style={styles.back}
+                onPress={() => navigation.navigate('Card1')}>
+                <Image
+                  style={styles.logo}
+                  source={require('../assets/snack-icon.png')}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity
+                style={styles.back}
+                onPress={() => navigation.navigate('Card1')}>
+                <Image
+                  style={styles.logo}
+                  source={require('../assets/snack-icon.png')}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate()}>
+                <Image
+                  style={styles.logo}
+                  source={require('../assets/snack-icon.png')}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <Text style={{ marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
+            {' '}
+            <Entypo name="location-pin" size={20} color="black" />
+            Hospital das clinicas
+          </Text>
+          <Text style={{ marginBottom: 10 }}>
+            The idea with React Native Elements is more about component
+            structure than actual design.
+          </Text>
+          <Text style={{ marginBottom: 10, fontWeight: 'bold' }}>
+            {' '}
+            <AntDesign name="like2" size={20} color="black" /> 30{' '}
+            <FontAwesome name="commenting" size={24} color="black" /> 4
+          </Text>
+        </Card>
         <Card>
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.navigate('Card1')}>
+          <Text
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginLeft: 10,
+              marginTop: 20,
+              marginBottom: 20,
+              fontWeight: 'bold',
+            }}>
             <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-          </TouchableOpacity>
-          <View></View>
-        </Card>
+              style={styles.image}
+              source={require('../assets/joel.png')}
+            />{' '}
+            Joel Miller
+          </Text>
 
-        <Card>
-          <TouchableOpacity onPress={() => navigation.navigate()}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-          </TouchableOpacity>
-          <View></View>
-        </Card>
-      </View>
-      <Text style={{ marginBottom: 10, fontWeight: 'bold', }}> <Entypo name="location-pin" size={20} color="black" />Hospital das clinicas</Text>
-      <Text style={{ marginBottom: 10 }}>
-        The idea with React Native Elements is more about component
-        structure than actual design.
-      </Text>
-      <Text style={{ marginBottom: 10, fontWeight: 'bold', }}>  <AntDesign name="like2" size={20} color="black" /> 30  <FontAwesome name="commenting" size={24} color="black" /> 4</Text>
-      <Card.Divider />
+          <Card.Divider />
+          <View style={styles.container}>
+            <View>
+              <TouchableOpacity
+                style={styles.back}
+                onPress={() => navigation.navigate('Card1')}>
+                <Image
+                  style={styles.logo}
+                  source={require('../assets/snack-icon.png')}
+                />
+              </TouchableOpacity>
+            </View>
 
-    </ScrollView>
+            <View>
+              <TouchableOpacity
+                style={styles.back}
+                onPress={() => navigation.navigate('Card1')}>
+                <Image
+                  style={styles.logo}
+                  source={require('../assets/snack-icon.png')}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate()}>
+                <Image
+                  style={styles.logo}
+                  source={require('../assets/snack-icon.png')}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <Text style={{ marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
+            {' '}
+            <Entypo name="location-pin" size={20} color="black" />
+            Hospital das clinicas
+          </Text>
+          <Text style={{ marginBottom: 10 }}>
+            The idea with React Native Elements is more about component
+            structure than actual design.
+          </Text>
+          <Text style={{ marginBottom: 10, fontWeight: 'bold' }}>
+            {' '}
+            <AntDesign name="like2" size={20} color="black" /> 30{' '}
+            <FontAwesome name="commenting" size={24} color="black" /> 4
+          </Text>
+        </Card>
+      </ScrollView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingBottom: 10,
   },
   logo: {
     height: 50,
     width: 50,
   },
   image: {
-    height: 33,
-    width: 31,
+    height: 35,
+    width: 25,
     marginBottom: -10,
     marginLeft: 10,
-  }
-},
-);
+  },
+});
 
 export default Feed1;
