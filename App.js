7266351202d984a62c387903a07812bc15dedde2
Harlei Akira Ohta <harlei.akira@outlook.com>
2023-03-08 15:22:11 -0300

    Tabnav

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Image } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
 import styles from './styles/MainStyle';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from './screens/Login';
@@ -10,29 +10,29 @@ import Principal from './screens/Principal';
 import Card1 from './screens/Card1';
 import Feed1 from './screens/Feed1';
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
-function MyStack() {
+function Tabs() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} options={{ title: 'Login' }}  />    
-      <Stack.Screen name="Principal" component={Principal} options={{ title: 'Principal' }}  />    
-      <Stack.Screen name="Card1" component={Card1} options={{ title: 'Hospital São Pedro' }}  />              
-      <Stack.Screen name="Feed1" component={Feed1} options={{ title: 'Hospital Feed' }}  />                      
-      
-    </Stack.Navigator>
-  );
+      <Tab.Navigator>
+      <Tab.Screen name="Principal" component={Principal}/> 
+      <Tab.Screen name="Card1" component={Card1} options={{ title: 'Hospital São Pedro' }} />            
+      <Tab.Screen name="Feed1" component={Feed1} options={{ title: 'Hospital Feed' }}  />         
+    </Tab.Navigator>
+  )
 }
 
-
-
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
-}
-
-
+  export default function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Login' }} />    
+        <Stack.Screen name="Principal" component={Tabs} options={{ title: 'Principal' }} />    
+        <Stack.Screen name="Card1" component={Card1} options={{ title: 'Hospital São Pedro' }} />            
+        <Stack.Screen name="Feed1" component={Feed1} options={{ title: 'Hospital Feed' }}  />   
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
