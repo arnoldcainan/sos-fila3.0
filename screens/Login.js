c75624642d794112406f20d7a06b99cdd3a315c9
arnoldcainan <86830636+arnoldcainan@users.noreply.github.com>
2023-03-08 21:14:02 -0300

    Update Login.js
    desenvolvimento de logica (login)

--- screens/Login.js
+++ screens/Login.js
@@ -10,8 +10,39 @@ export default function Login({ navigation }) {
 
   const [email, setEmail] = useState(null)
   const [password, setPassword] = useState(null)
+  //logica de login
+  const [errorEmail, setErrorEmail] = useState(null)
+  const [errorPassword, setErrorPassword] = useState(null)
+
+  const validar = ()=> {
+    let error = false
+    setErrorEmail(null)
+    setErrorPassword(null)
+
+    //verifica atraves de REGEX formatação do email (ex teste@teste.com)
+    const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+
+
+    if (!re.test(String(email).toLowerCase())){
+      setErrorEmail('Preencha seu email corretamente')
+      error = true
+    }
+
+     if (password == null){
+      setErrorPassword('Preencha sua senha corretamente')
+      error = true
+    }
+
+    
+    return !error
+
+  }
+ 
 
   const entrar = () => {
+    if(validar())
     navigation.reset({
       index: 0,
       routes: [{ name: "Principal" }]
@@ -29,15 +60,23 @@ export default function Login({ navigation }) {
         placeholder="  E-mail"
         placeholderTextColor="#24757B"
         leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-        onChangeText={value => setEmail(value)}
+        onChangeText={value => {
+        setEmail(value)
+        setErrorEmail(null)
+        }}
         keyboardType='email-address'
+        errorMessage={errorEmail}
       />
       <Input style={styles.inputView}
         placeholder="Senha"
         placeholderTextColor="#24757B"
         leftIcon={{ type: 'font-awesome', name: 'lock' }}
-        onChangeText={value => setPassword(value)}
+        onChangeText={value => {
+          setPassword(value)
+          setErrorPassword(null)
+        }}
         secureTextEntry={true}
+        errorMessage={errorPassword}
       />
 
       <Button style={styles.button}
@@ -55,5 +94,3 @@ export default function Login({ navigation }) {
     </View>
   );
 }
-
-
